2091aeae1bd0fd5c21f2279a11cecb8fc41fb2ed
Oleg Valiulin <liam141810@gmail.com>
2025-06-04 17:26:09 +0100

    add root privs

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --name jupyter -p 8888:8888 -v `pwd`/notebooks:/opt/notebooks jupyter/base-notebook jupyter notebook --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root
+# Running container with root saves from Permission Denied errors: https://github.com/jupyter/docker-stacks/issues/1187
+
+docker run --user root --name jupyter -p 8888:8888 -v `pwd`/notebooks:/opt/notebooks jupyter/base-notebook\
+jupyter notebook --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root
