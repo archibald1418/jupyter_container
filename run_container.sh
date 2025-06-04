2efd7b085c362ac43493417b59cfacde75571f53
Oleg Valiulin <liam141810@gmail.com>
2025-06-04 16:32:36 +0100

    update container name
    `jupyter:latest` became private, change to new basic image

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name jupyter -p 8888:8888 -v `pwd`/notebooks:/opt/notebooks jupyter jupyter notebook --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root
+docker run --name jupyter -p 8888:8888 -v `pwd`/notebooks:/opt/notebooks jupyter/base-notebook jupyter notebook --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root
